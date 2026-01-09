5fc2a12f6a3133a76160f752596d8ee92a749a19
Matrodazz <matrodazzbuilds@gmail.com>
2026-01-09 13:13:21 -0300

    feat: light and dark theme button

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const body = document.body;
+    const button = document.getElementById("theme__toggle");
+    const icon = document.getElementById("theme__icon");
+    const logo = document.getElementById("logo");
+
+    if (button && icon) {
+        button.addEventListener("click", () => {
+            const isDark = body.getAttribute("data-theme") === "dark";
+            body.setAttribute("data-theme", isDark ? "light" : "dark");
+            icon.src = isDark ? "/img/darktheme.svg" : "/img/lighttheme.svg";
+            logo.src = isDark ? "/img/logoblack.png" : "/img/logowhite.png";
+        });
+    }
+
+});
